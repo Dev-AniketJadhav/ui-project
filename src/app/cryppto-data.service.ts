77c5578313effd294e5a45dd775d9823b55ed85c
Aniket <jadhavaniket0118@gmail.com>
2022-05-24 17:43:07 +0530

    third commit with api

--- src/app/cryppto-data.service.ts
+++ src/app/cryppto-data.service.ts
@@ -10,9 +10,15 @@ export class CrypptoDataService {
   constructor(private http: HttpClient) {}
 
   GetCrypto() {
-    return this.http.get(
-      'https://api.nomics.com/v1/currencies/ticker?key=your-key-here f6d84225ab4738000322a6b8f80ae20934dd37bb&ids=BTC,ETH,XRP&interval=1d,30d&convert=EUR&platform-currency=ETH&per-page=100&page=1'
-    );
+    // return this.http.get(
+    //   'https://api.nomics.com/v1/currencies/ticker?key=f6d84225ab4738000322a6b8f80ae20934dd37bb&ids=BTC,ETH,XRP&interval=1d,30d&convert=EUR&platform-currency=ETH&per-page=100&page=1'
+    // );
+    return this.http.get('https://api.coinstats.app/public/v1/coins?skip=0');
   }
+
+  // GetCryptoHistory(){
+  //   return this.http.get('')
+  // }
 }
+
 //3bc7c47d-d7b5-4de2-8502-8a8f1017b9c1
